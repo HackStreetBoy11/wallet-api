728e255574906d8c846e3b62472b71ee256bcc0b
myProject <your-email@example.com>
2025-10-04 19:57:50 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -5,14 +5,21 @@ dotenv.config();
 
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import job from './config/cron.js';
 const app = express();
+if(process.env.NODE_ENV === 'production'){
+job.start(); // Start the cron job
+}
 const PORT = process.env.PORT || 5001
 app.use(rateLimiter);
 app.use(express.json())
 
 
-
+app.use("/api/health", (req, res) => {
+    res.status(200).send("API is healthy");
+});
 app.use("/api/transaction", transactionsRouter);
+
 
 
 // Start the server after initializing the database
